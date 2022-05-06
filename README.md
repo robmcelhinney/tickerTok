2fe92394c5441045bf0afc21798b7125e13664d3
Robert McElhinney <robmcelhinney@hotmail.com>
2022-05-06 19:14:15 +0100

    Update tickerTok with gist changes
    Hide holdings if not provided
    Update README

---
+++ README.md
@@ -0,0 +1,42 @@
+# TickerTok
+
+Simple command line portfolio tracker
+
+## Description
+
+Python script that returns table with stocks/cryptos
+
+## Getting Started
+
+
+### Executing program
+
+
+```sh
+> python3 tickerTok.py -f ticker.yaml 
+> python3 tickerTok.py -w ETH-USD,XM,AAPL
+```
+
+To auto update use `watch`
+```sh
+> watch -t -c python3 tickerTok.py -f ticker.yaml
+```
+```sh
+> watch -t -c python3 tickerTok.py -f holdings.yaml
+```
+
+## Help
+
+Any advise for common problems or issues.
+```
+> python3 tickerTok.py -h
+```
+
+## License
+
+This project is licensed under the GNU Lesser General Public License v3.0
+
+
+## Acknowledgments
+
+Using the Yahoo Finance API
--- tickerTok.py
+++ tickerTok.py
@@ -3,13 +3,10 @@
 from rich.console import Console
 from rich.table import Table
 from rich import box
-
 import sys, argparse, logging
-
 import requests
-import json
 import math
-
+import time
 import yaml
 
 
@@ -25,7 +22,6 @@ def millify(n):
 
 API_ENDPOINT="https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
 fields = ["symbol", "marketState", "regularMarketPrice", "regularMarketChange", "regularMarketChangePercent", "displayName", "marketCap"]
-# symbols = ["BBBY", "GME", "AAPL"]
 
 # ex = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=AAPL,NFLX"
 # fulldata = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=BBBY,GME"
@@ -44,25 +40,30 @@ def apiCall(symbols):
         if i != len(symbols) - 1:
             symbolParameters += ","
 
+    # Get a copy of the default headers that requests would use
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-agent': 'Mozilla/5.0',
+            'From': 'yahoofinanceapi@example.com'  # This is another valid field
+        }
+    )
 
-    # print("URL: ", "{}&fields={}&symbols={}".format(API_ENDPOINT, fieldParameters, symbolParameters))
-
-    r = requests.get("{}&fields={}&symbols={}".format(API_ENDPOINT, fieldParameters, symbolParameters))
-    # r.status_code
+    url = "{}&fields={}&symbols={}".format(API_ENDPOINT, fieldParameters, symbolParameters)
+    logging.debug("url: {}".format(url))
+    r = requests.get(url, headers=headers)
+    logging.debug("status_code: {}".format(r.status_code))
 
     quoteResponse = r.json()["quoteResponse"]
-    # quoteResponse = json["quoteResponse"]
     error = quoteResponse["error"]
     if error:
         logging.error("WARNING ERROR!!!!")
 
     results = quoteResponse["result"]
-    # print("results: ", results)
     return results
 
 
-
-def createTable(data):
+def createTable(data, holdings=None):
     console = Console()
 
     table = Table(show_header=True, header_style="bold magenta", box=box.ROUNDED)
@@ -72,16 +73,20 @@ def createTable(data):
     table.add_column("Percent", justify="right")
     table.add_column("Change", justify="right")
     table.add_column("Market Cap", justify="right")
+    if holdings:
+        table.add_column("Holdings", justify="right")
     table.add_column(":thumbs_up:/:thumbs_down:")
 
-    for ticker in data:
-        # print("ticker: ", ticker)
+    totalHoldings = 0
+    totalChange = 0
+    totalChangePercent = 0
 
+    for ticker in data:
         changePercent = ticker["regularMarketChangePercent"]
         change = ticker["regularMarketChange"]
         
         changePercentStr = "{:.2f}".format(changePercent)
-        changeStr = "{:.2f}".format(change)
+        changeStr = "{:,.2f}".format(change)
         if changePercent > 0:
             changePercentStr = "[green3]{}%[green3]".format(changePercentStr)
             changeStr = "[green3]{}[green3]".format(changeStr)
@@ -89,7 +94,6 @@ def createTable(data):
             changePercentStr = "[red1]{}%[red1]".format(changePercentStr.replace("-", ""))
             changeStr = "[red1]{}[red1]".format(changeStr.replace("-", ""))
         
-        emoji = ""
         if changePercent > 10:
             emoji = ":rocket:"
         elif changePercent < -10:
@@ -99,27 +103,61 @@ def createTable(data):
         
         marketCap = millify(ticker["marketCap"])
 
-        # logging.debug("ticker: %s" % ticker)
-
+        logging.debug("ticker: {}".format(ticker))
         displayNameStr = ticker["symbol"]
         if "displayName" in ticker:
             displayNameStr = str(ticker["displayName"])
+    
+        if holdings:
+            logging.debug("holdings: {}".format(holdings))
+            holding = float(holdings[ticker["symbol"]])
+            holdingValue = holding * float(ticker["regularMarketPrice"])
+            totalHoldings += holdingValue
+            logging.debug("holdingValue: {}".format(holdingValue))
+            holdingRow = "{:,.2f}".format(holdingValue)
 
-        table.add_row(
-            ticker["symbol"],
-            displayNameStr,
-            str(ticker["regularMarketPrice"]),
-            changePercentStr,
-            changeStr,
-            marketCap,
-            emoji
-        )
+            table.add_row(
+                ticker["symbol"],
+                displayNameStr,
+                "{:.2f}".format(ticker["regularMarketPrice"]),
+                changePercentStr,
+                changeStr,
+                marketCap,
+                holdingRow,
+                emoji,
+            )
 
+            totalChange += change * holding
+            totalChangePercent = (totalChange / (totalHoldings - totalChange)) * 100
+
+            if totalChange >= 0:
+                console.print("Daily Change: [green3]~${:.2f}[green3]".format(totalChange))
+                console.print("Daily % Change: [green3]~{:.2f}%[green3]".format(totalChangePercent))
+            else:
+                console.print("Daily Change: [red1]~${:.2f}[red1]".format(totalChange))
+                console.print("Daily % Change: [red1]~{:.2f}%[red1]".format(totalChangePercent))
+            console.print("Daily Change Percent: ~${:.2f}".format(totalChangePercent))
+            console.print("Total Holdings: [bold dodger_blue1]~${:.2f}[bold dodger_blue1]".format(totalHoldings))
+        else:
+            table.add_row(
+                ticker["symbol"],
+                displayNameStr,
+                "{:.2f}".format(ticker["regularMarketPrice"]),
+                changePercentStr,
+                changeStr,
+                marketCap,
+                emoji,
+            )
     console.print(table)
+
+def refreshDataTable(watchlist, holdings):
+    data = apiCall(watchlist)
+    logging.debug("data: {}".format(data))
+    createTable(data, holdings)
 
 # Gather our code in a main() function
 def main(args):
-    logging.debug("Your Argument: %s" % args)
+    logging.debug("Your Argument: {}".format(args))
 
     if not args.watchlist and not args.file:
         logging.error("Must give watchlists as parameter or file")
@@ -128,41 +166,44 @@ def main(args):
     watchlist = []
     if args.watchlist:
         watchlist = (args.watchlist).split(",")
-        logging.debug("watchlist: %s" % watchlist)
+        logging.debug("watchlist: {}".format(watchlist))
 
+    holdings = None
     file = (args.file)
     if file:
         try:
             yamlFile = open(file)
             yamlFile = yaml.load(yamlFile, Loader=yaml.FullLoader)
-            yamlWatchlist = yamlFile["watchlist"]
-
-            watchlist = list(set(watchlist + yamlWatchlist))
-
+            if "watchlist" in yamlFile:
+                yamlWatchlist = yamlFile["watchlist"]
+                watchlist = list(set(watchlist + yamlWatchlist))
+            elif "holdings" in yamlFile:
+                holdings = yamlFile["holdings"]
+                watchlist = set(holdings.keys())
         except IOError as err:
             logging.error("Could not open file {}. Error: {}".format(file, err))
             sys.exit()
         except yaml.YAMLError as err:
             logging.error("Could not load yaml from file {}. Error: {}".format(file, err))
             sys.exit()
-        
+
     watchlist = sorted(watchlist)
-    data = apiCall(watchlist)
-    logging.debug("data: %s" % data)
-    createTable(data)
+
+    if (args.interval):
+        while True:
+            data = apiCall(watchlist)
+            logging.debug("data: {}".format(data))
+            print(chr(27) + "[2J")
+            createTable(data, holdings)
+            time.sleep(args.interval)
+    refreshDataTable(watchlist, holdings)
 
 
-
-
-
-# Standard boilerplate to call the main() function to begin
-# the program.
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-                                    description = "Tracks stocks using yahoo finance api.",
-                                    epilog = "As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
-                                    fromfile_prefix_chars = '@' )
-    # TODO Specify your real parameters here.
+            description = "Tracks stocks using yahoo finance api.",
+            epilog = "As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
+            fromfile_prefix_chars = '@' )
     parser.add_argument(
             "-w"
             "--watchlist",
@@ -176,6 +217,14 @@ if __name__ == '__main__':
             dest="file",
             action="store",
             help="file storing watchlist (yaml)", 
+            default=None)
+    parser.add_argument(
+            "-i",
+            "--interval",
+            dest="interval",
+            action="store",
+            type=int,
+            help="interval to refresh table", 
             default=None)
     parser.add_argument(
             "-v",
